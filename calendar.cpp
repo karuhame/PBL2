9a0b2515353b96c94eba741ff0c22398bb50e8cf
karuhame <62510010+karuhame@users.noreply.github.com>
2022-09-30 06:55:42 +0700

    Create calendar.cpp

---
+++ calendar.cpp
@@ -0,0 +1,52 @@
+
+struct info{
+    char hoten[20];
+    char sdt[10];
+    
+};
+
+struct TimeDay{
+    //5h - 20h
+    int daCoc[17] = {};
+};
+
+enum DaysOfWeek
+{
+	SUNDAY,
+	MONDAY,
+	TUESDAY,
+	WEDNESDAY,
+	THURSDAY,
+	FRIDAY,
+	SATURDAY
+};
+
+
+struct TimeDay week[8];
+
+class calendar{
+    private:
+        int thu;
+        struct info thongtin;
+        struct TimeDay ngay;
+    
+    public:
+
+        // Them chuc nang thoi gian    
+        void hienThiThongTin(){
+            cout<< "Ho va ten:"<< thongtin.hoten<< endl;
+            cout<<" SDT:"<< thongtin.sdt<<endl;
+        //...
+        }
+
+        //Cap nhat ngay vao danh sach tuan
+        void tao(){
+            week[thu] = day;
+        }
+
+        //dat san
+        void datSan(struct timeDay t, int x){
+            t.daCoc[x] = 1;
+        }
+
+}   
